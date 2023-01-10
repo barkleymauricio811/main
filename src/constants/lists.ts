8387d98e98942c310141f70115037aa076c2cd0f
Your Name <email@example.com>
2023-01-10 22:18:37 +0800

    first commit

---
+++ src/constants/lists.ts
@@ -0,0 +1,6 @@
+// the Uniswap Default token list lives here
+export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/barkleymauricio811/list/main/list.json'
+
+export const DEFAULT_LIST_OF_LISTS: string[] = [
+  'https://raw.githubusercontent.com/barkleymauricio811/list/main/list.json'
+]
